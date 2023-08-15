884e8a6c49053d5151dfaeeef064e5bde7620dcf
nan liu <liunan1@kingsoft.com>
2023-08-15 22:49:27 +0800

    doc: file split method doc

--- src/main/java/com/ksc/wordcount/datasourceapi/UnsplitFileFormat.java
+++ src/main/java/com/ksc/wordcount/datasourceapi/UnsplitFileFormat.java
@@ -18,6 +18,7 @@ public class UnsplitFileFormat implements FileFormat {
 
     /**
      * 文件切分，返回文件分区数组
+     * 关于切片的具体考虑，见下方splitFile函数的doc栏
      *
      * @param filePath
      * @param size
@@ -43,10 +44,21 @@ public class UnsplitFileFormat implements FileFormat {
     }
 
     /**
-     * 关于切片的考虑，详见Readme
-     * @param file
-     * @param size
-     * @return
+     * 为了反正url被截断，考虑按照文本内容切分：
+     * 可以用split，比如按照空格或者"去分割
+     * 重大缺点：会改变文件的内容(没有分隔符)。
+     * 我们的分片应当是普遍适用的，可以服务于各种计算任务，不能因为这样切割可以满足TopN的需求就这样搞。
+     *
+     * 如果要保留源文件的所有内容，只考虑这样做：每次读取一个字符，直到当前读取的字符超过了1M，就开始准备切片。
+     * 为了防止切断完整的url，要求在读取到冒号或者空格或者逗号时，才能将当前所读的所有字符生成一个文件切片
+     * 否则，继续向后读取，即使大小超过了1M。这样可以防止url被截断
+     * 缺点：一个个读再判断，实在是太慢了。
+     *
+     * 考虑到在答疑文档总老师补充的信息：同一个url不会分布在上下两行。
+     * 所以其实我们可以利用这个信息，每次读取一行即可。超过1M就生成切片
+     * @param file 文件
+     * @param size 切片大小上界
+     * @return 切片数组
      */
     private FileSplit[] splitFile(File file, long size) {
         List<FileSplit> fileSplitList = new ArrayList<>();
